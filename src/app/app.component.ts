ac333dfb11136caf2c3412cb3871fbad55633549
Steven Chalk <steven.chalk@parentpay.com>
2025-11-13 10:49:24 +0000

    Files update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { filter, takeUntil } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'Angular Modules Sample - MSAL Angular';
+  title = 'Chalks Angular Modules Sample - MSAL Angular';
   isIframe = false;
   loginDisplay = false;
   private readonly _destroying$ = new Subject<void>();
